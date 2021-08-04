1caad919e3802f1eacad3416617b42c0fd01a2f1
Joshfishman97 <fishmanjosh251@gmail.com>
2021-08-04 22:35:13 +0000

    another one

--- src/js/views/PeoplePage.js
+++ src/js/views/PeoplePage.js
@@ -6,10 +6,24 @@ export function PeoplePage() {
 	useEffect(() => {}, []);
 	fetch("https://www.swapi.tech/api/people")
 		.then(res => res.json())
-		.then(data => setPeople(data.result))
+		.then(data => setPeople(data.results))
 		.catch(err => console.log(err));
 	return (
-		<div>
+		<div className="row">
+			{people.map((item, index) => {
+				return (
+					<div key={index} className="card col-3" style={{ width: "16rem" }}>
+						<img className="card-img-top" src=".../100px180/" alt="Card image cap" />
+						<div className="card-body">
+							<h5 className="card-title">{item.name}</h5>
+							<p className="card-text" />
+							<a href="#" className="btn btn-primary">
+								Go somewhere
+							</a>
+						</div>
+					</div>
+				);
+			})}
 			<h1>People Page</h1>
 		</div>
 	);
