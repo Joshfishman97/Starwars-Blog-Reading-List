83eead1e518ad05d46a2b10647404339504280fa
Joshfishman97 <fishmanjosh251@gmail.com>
2021-08-05 23:47:26 +0000

    another

--- src/js/views/Vehicles.js
+++ src/js/views/Vehicles.js
@@ -19,7 +19,7 @@ export function Vehicles() {
 						<div className="card-body">
 							<h5 className="card-title">{item.name}</h5>
 							<p className="card-text" />
-							<a href="#" className="btn btn-primary">
+							<a href={"/vehicles/" + item.uid} className="btn btn-primary">
 								Go somewhere
 							</a>
 						</div>
--- src/js/views/VehiclesIndividual.js
+++ src/js/views/VehiclesIndividual.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 export function VehiclesIndividual() {
 	const params = useParams();
+
+	const [details, setDetails] = useState(null);
+	useEffect(
+		() => {
+			fetch(`https://www.swapi.tech/api/vehicles/${params.id}`)
+				.then(res => res.json())
+				.then(data => setDetails(data.result))
+				.catch(err => console.log(err));
+		},
+		[params.id]
+	);
+
 	return (
 		<div>
-			<h1>
-				vehicles
-				{params.id}
-			</h1>
+			<h1>{details !== null ? details.properties.model : null}</h1>
 		</div>
 	);
 }
