cbec03fa4801e96db71601eed7eabfbc819a29f0
Joshfishman97 <fishmanjosh251@gmail.com>
2021-08-07 17:00:15 +0000

    Another one

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -11,6 +11,35 @@ export const Navbar = () => {
 				<Link to="/demo">
 					<button className="btn btn-primary">Check the Context in action</button>
 				</Link>
+				<div className="dropdown">
+					<a
+						className="btn btn-secondary dropdown-toggle"
+						href="#"
+						role="button"
+						id="dropdownMenuLink"
+						data-bs-toggle="dropdown"
+						ariaExpanded="false">
+						Dropdown link
+					</a>
+
+					<ul className="dropdown-menu show" ariaLabelledBy="dropdownMenuLink">
+						<li>
+							<a className="dropdown-item" href="#">
+								Action
+							</a>
+						</li>
+						<li>
+							<a className="dropdown-item" href="#">
+								Another action
+							</a>
+						</li>
+						<li>
+							<a className="dropdown-item" href="#">
+								Something else here
+							</a>
+						</li>
+					</ul>
+				</div>
 			</div>
 		</nav>
 	);
